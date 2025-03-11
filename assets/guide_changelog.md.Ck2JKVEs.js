2c452e135346cc2139fbc31d7d4d216ecf0abf00
xuyouer <xuyouer@users.noreply.github.com>
2025-03-11 18:04:18 +0000

    deploy: 94f963409cd84dadfd473e3ac19e661eb0b21a8c

---
+++ assets/guide_changelog.md.Ck2JKVEs.js
@@ -0,0 +1,94 @@
+import{_ as a,c as i,o as n,am as l}from"./chunks/framework.cYnJl0hF.js";const g=JSON.parse('{"title":"变更日志","description":"","frontmatter":{},"headers":[],"relativePath":"guide/changelog.md","filePath":"guide/changelog.md","lastUpdated":1741716162000}'),p={name:"guide/changelog.md"};function h(e,s,t,k,E,d){return n(),i("div",null,s[0]||(s[0]=[l(`<h1 id="变更日志" tabindex="-1">变更日志 <a class="header-anchor" href="#变更日志" aria-label="Permalink to &quot;变更日志&quot;">​</a></h1><h2 id="toc" tabindex="-1">TOC <a class="header-anchor" href="#toc" aria-label="Permalink to &quot;TOC&quot;">​</a></h2><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">01.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Avatar</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">02.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Badge</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">03.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 BackTop</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">04.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Button</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">05.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Card</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">06.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 Carousel</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">07.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Code</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">08.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 Col</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">09.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Cover</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">10.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 Dice</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">11.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Divider</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">12.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 Empty</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">13.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 GlobalFooter</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">14.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Icon</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">15.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 Image</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">16.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Link</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">17.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Numeral</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">18.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 PageHeader</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">19.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 Pagination</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">20.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 Poster</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">21.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Progress</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">22.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ QrCode</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">23.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ RandomText</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">24.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 Result</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">25.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 Row</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">26.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Space</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">27.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 Steps</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">28.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 Tag</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">29.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Text</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">30.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ✅ Time</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">31.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 Timeline</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">32.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 🔲 WordCount</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    ... ...</span></span></code></pre></div><h2 id="_1-0-8-20250312" tabindex="-1">1.0.8 - 20250312 <a class="header-anchor" href="#_1-0-8-20250312" aria-label="Permalink to &quot;1.0.8 - 20250312&quot;">​</a></h2><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">更新:</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyProgress 水波wave类型进度条</span></span></code></pre></div><h2 id="_1-0-7-20250311" tabindex="-1">1.0.7 - 20250311 <a class="header-anchor" href="#_1-0-7-20250311" aria-label="Permalink to &quot;1.0.7 - 20250311&quot;">​</a></h2><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">新增:</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyProgress</span></span></code></pre></div><h2 id="_1-0-6-20250218" tabindex="-1">1.0.6 - 20250218 <a class="header-anchor" href="#_1-0-6-20250218" aria-label="Permalink to &quot;1.0.6 - 20250218&quot;">​</a></h2><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">新增:</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyNumeral</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">更新:</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 第三方库工具类</span></span></code></pre></div><h2 id="_1-0-5-20250218" tabindex="-1">1.0.5 - 20250218 <a class="header-anchor" href="#_1-0-5-20250218" aria-label="Permalink to &quot;1.0.5 - 20250218&quot;">​</a></h2><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">新增:</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyRandomText</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">更新:</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 工具类重构</span></span></code></pre></div><h2 id="_1-0-4-20250217" tabindex="-1">1.0.4 - 20250217 <a class="header-anchor" href="#_1-0-4-20250217" aria-label="Permalink to &quot;1.0.4 - 20250217&quot;">​</a></h2><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">更新:</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 组件命名规范</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">2.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 组件types注释规范</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">3.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyPoster灰度测试</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">4.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyTime自定义类名和样式</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">5.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyCover自定义类名和样式</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">TODO:</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyRow/xyCol</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">2.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyImage</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">3.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyTag</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">4.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyProgress</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">5.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyNumeral</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">6.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyRandomText</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">7.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ... ...</span></span></code></pre></div><h2 id="_1-0-3-20250215" tabindex="-1">1.0.3 - 20250215 <a class="header-anchor" href="#_1-0-3-20250215" aria-label="Permalink to &quot;1.0.3 - 20250215&quot;">​</a></h2><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">更新:</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyTime组件</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">2.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> Utils模块化</span></span></code></pre></div><h2 id="_1-0-2-20250214" tabindex="-1">1.0.2 - 20250214 <a class="header-anchor" href="#_1-0-2-20250214" aria-label="Permalink to &quot;1.0.2 - 20250214&quot;">​</a></h2><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">更新：</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyQrCode组件</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">2.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyAvatar组件 size 属性</span></span></code></pre></div><h2 id="_1-0-1" tabindex="-1">1.0.1 <a class="header-anchor" href="#_1-0-1" aria-label="Permalink to &quot;1.0.1&quot;">​</a></h2><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">更新:</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyAvatar, xyAvatarGroup</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">2.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyBadge</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">3.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyButton, xyButtonGroup</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">4.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyCard</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">5.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyCode</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">6.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyCover</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">7.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyDivider</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">8.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyIcon</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">9.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyLink</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">10.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xySpace</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">11.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> xyText</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">修复:</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 已知问题</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">其它:</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 上传Github</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">2.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 上传NPM</span></span></code></pre></div><h2 id="_1-0-0" tabindex="-1">1.0.0 <a class="header-anchor" href="#_1-0-0" aria-label="Permalink to &quot;1.0.0&quot;">​</a></h2><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">更新:</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 初始化项目</span></span></code></pre></div>`,21)]))}const c=a(p,[["render",h]]);export{g as __pageData,c as default};
